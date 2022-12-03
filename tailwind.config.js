18f6df93b264af024938c6474eb76484c479a360
Israel Medina <israel.medina@maritz.com>
2022-12-03 12:05:05 -0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,vue,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
