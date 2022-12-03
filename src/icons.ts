c43953bec1d14219d84b3c7d6f53f36260a4c5e8
Israel Medina <israel.medina@maritz.com>
2022-12-03 12:28:31 -0300

    feat(icons): added font awesome support

---
+++ src/icons.ts
@@ -0,0 +1,9 @@
+export {
+  faYoutube,
+  faInstagram,
+  faInstagramSquare,
+  faTwitter,
+  faTwitterSquare,
+  faPatreon,
+  faTwitch,
+} from "@fortawesome/free-brands-svg-icons";
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,19 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import "./assets/styles/main.css";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+import * as icons from "./icons";
+
 import App from "./App.vue";
 import router from "./router";
 
 const app = createApp(App);
+
+library.add(icons);
+
+app.component("FontAwesomeIcon", FontAwesomeIcon);
 
 app.use(createPinia());
 app.use(router);
