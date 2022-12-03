e8f222009012b628a4f6679e3a53f0b0220b9251
Israel Medina <isra2dave@gmail.com>
2022-12-03 12:05:05 -0300

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import DaveLogo from "../components/DaveLogo.vue";
+</script>
+<template>
+  <main class="bg-black h-screen flex items-center justify-center">
+    <div class="w-full px-5 md:px-20 lg:px-40">
+      <DaveLogo />
+    </div>
+    <div></div>
+  </main>
+</template>
