cdfb24d6f9d4a62b8f3aec2796ad9bd2a8bc3754
Israel Medina <isra2dave@gmail.com>
2022-12-05 11:23:59 -0300

    feat(videoItem): update styling

---
+++ src/components/VideoItem.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { formatDuration } from "@/utils";
+const props = defineProps({
+  video: Object,
+});
+</script>
+<template>
+  <a
+    :href="props.video?.media.url"
+    class="block relative w-80 border border-gray-700 rounded-sm overflow-hidden"
+  >
+    <span class="font-bold text-sm absolute">{{ props.video?.title }}</span>
+    <div v-if="props.video?.thumbnail" class="z-0">
+      <img
+        :src="props.video?.thumbnail.url"
+        :alt="`${props.video?.title} thumbnail`"
+      />
+    </div>
+    <div
+      class="absolute z-20 right-0 bottom-0 px-2 text-sm font-semibold bg-black/70"
+    >
+      {{ formatDuration(props.video?.duration) }}
+    </div>
+  </a>
+</template>
