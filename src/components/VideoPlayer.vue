37b8b7d2cf88f032397d8babbd495e683603b716
Israel Medina <israel.medina@maritz.com>
2023-06-26 15:03:30 -0300

    fix: remove muted video attr

--- src/components/VideoPlayer.vue
+++ src/components/VideoPlayer.vue
@@ -1,5 +1,5 @@
 <template>
-  <video class="w-full h-full" autoplay controls :src="video.url" muted></video>
+  <video class="w-full h-full" autoplay controls :src="video.url"></video>
 </template>
 <script setup lang="ts">
 import type { Video } from "@/types";
