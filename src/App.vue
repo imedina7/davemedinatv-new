e389cf87776ed4f26c5d07562ca5ffcce2e63a51
Israel Medina <isra2dave@gmail.com>
2023-11-27 23:33:34 -0300

    chore(cleanup): remove socket.io

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,7 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
-import { state } from "@/socket";
-import { computed } from "vue";
-
-const connected = computed(() => state.connected);
 </script>
 
 <template>
   <RouterView />
-  <div style="color: white">Connected: {{ connected }}</div>
 </template>
