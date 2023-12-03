4bb7f0518abb62ec71cc622ace7f3bd7609b6857
Israel Medina <isra2dave@gmail.com>
2023-12-03 16:17:51 -0300

    feat(stream): added stream floating window

--- src/components/LivePlayer.vue
+++ src/components/LivePlayer.vue
@@ -1,12 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useAppStore } from "@/stores/app";
+import { computed } from "vue";
+
+const store = useAppStore();
+const VERCEL_URL = import.meta.env.VERCEL_URL;
+const TWITCH_LOGIN = import.meta.env.TWITCH_LOGIN;
+const isLive = computed(() => store.stream.isLive);
+</script>
 
 <template>
-  <iframe
-    title="Twitch Player"
-    src="https://player.twitch.tv/?davemedinatv&parent=www.davemedina.vercel.app"
-    height="<height>"
-    width="<width>"
-    allowfullscreen
-  >
-  </iframe>
+  <div v-if="isLive" class="fixed bottom-5 right-6 h-32">
+    <iframe
+      title="Twitch Player"
+      :src="`https://player.twitch.tv/?${TWITCH_LOGIN}&parent=${VERCEL_URL}`"
+      height="120"
+      width="212"
+      allowfullscreen
+    >
+    </iframe>
+  </div>
 </template>
