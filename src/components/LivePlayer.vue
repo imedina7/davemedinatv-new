acb01c12e68582852934cf644d00563f46a02a5b
Israel Medina <isra2dave@gmail.com>
2023-12-03 16:24:38 -0300

    fix(stream): hardcode stage domain

--- src/components/LivePlayer.vue
+++ src/components/LivePlayer.vue
@@ -3,7 +3,6 @@ import { useAppStore } from "@/stores/app";
 import { computed } from "vue";
 
 const store = useAppStore();
-const VERCEL_URL = import.meta.env.VUE_APP_VERCEL_URL;
 const TWITCH_LOGIN = import.meta.env.TWITCH_LOGIN;
 const isLive = computed(() => store.stream.isLive);
 </script>
@@ -12,7 +11,7 @@ const isLive = computed(() => store.stream.isLive);
   <div v-if="isLive" class="fixed bottom-5 right-6 h-32">
     <iframe
       title="Twitch Player"
-      :src="`https://player.twitch.tv/?${TWITCH_LOGIN}&parent=${VERCEL_URL}`"
+      :src="`https://player.twitch.tv/?${TWITCH_LOGIN}&parent=stage.davemedina.tv`"
       height="120"
       width="212"
       allowfullscreen
