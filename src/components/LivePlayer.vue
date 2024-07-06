4072c8222e6bbd05dd330184bf915388dbfc4956
Israel Medina <contact@israelmedina.com>
2024-07-06 17:15:03 -0300

    chore(twitch): update host

--- src/components/LivePlayer.vue
+++ src/components/LivePlayer.vue
@@ -1,17 +1,20 @@
 <script setup lang="ts">
-import classnames from "classnames";
-
-const TWITCH_LOGIN = import.meta.env.VITE_TWITCH_LOGIN;
+const TWITCH_LOGIN: string = import.meta.env.VITE_TWITCH_LOGIN;
+const BASE_URL: string = import.meta.env.VITE_BASE_URL.replace("https://");
+const host =
+  BASE_URL[BASE_URL.length - 1] === "/"
+    ? BASE_URL.substring(0, BASE_URL.length - 1)
+    : BASE_URL;
 </script>
 
 <template>
-  <div :class="classnames('-z-50 inset-0 w-screen h-screen')">
+  <div class="-z-50 inset-0 w-screen h-screen">
     <iframe
       title="Twitch Player"
-      :src="`https://player.twitch.tv/?channel=${TWITCH_LOGIN}&parent=stage.davemedina.tv`"
+      :src="`https://player.twitch.tv/?channel=${TWITCH_LOGIN}&parent=${host}`"
       width="100%"
       height="100%"
-      parent="stage.davemedina.tv"
+      :parent="host"
       allowfullscreen
     >
     </iframe>
