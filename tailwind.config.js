c357d7d5466364f4bef8d1291b588bf019ad44c8
Israel Medina <contact@israelmedina.com>
2025-02-28 16:50:48 -0300

    chore: use bun as package manager, change project to ES module by default

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 
-module.exports = {
+
+
+export default {
   content: ["./index.html", "./src/**/*.{js,ts,vue,jsx,tsx}"],
   theme: {
     extend: {
